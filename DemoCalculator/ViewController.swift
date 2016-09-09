9aba4861bd55767f2cdc28ab6010f45b46511a20
ttb10912 <ttb10912@TTB10912s-MacBook-Pro.local>
2016-09-09 11:34:44 +0700

    abc
    ..ac

--- DemoCalculator/ViewController.swift
+++ DemoCalculator/ViewController.swift
@@ -10,11 +10,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var textFieldToResultLabel: NSLayoutConstraint!
+    
+    @IBOutlet weak var resultLabelToMidView: NSLayoutConstraint!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    func updateContrain(){
+        let scale = UIScreen.mainScreen().bounds.size.height / 667
+        
+        resultLabelToMidView.constant = UIScreen.mainScreen().bounds.size.height > 480 ? resultLabelToMidView.constant * scale : resultLabelToMidView.constant * 0.1
+        
+        
+        textFieldToResultLabel.constant = UIScreen.mainScreen().bounds.size.height > 480 ? textFieldToResultLabel.constant * scale : textFieldToResultLabel.constant * 0.1
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
