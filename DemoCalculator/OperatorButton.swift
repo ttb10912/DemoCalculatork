9a8a5a117e37a491c198f0d6de24f5b5015000ae
ttb10912 <ttb10912@TTB10912s-MacBook-Pro.local>
2016-09-09 11:42:53 +0700

    .
    ..

---
+++ DemoCalculator/OperatorButton.swift
@@ -0,0 +1,21 @@
+//
+//  OperatorButton.swift
+//  DemoCalculator
+//
+//  Created by TTB10912 on 09/09/2016.
+//  Copyright © 2016 TTB10912. All rights reserved.
+//
+
+import UIKit
+
+class OperatorButton: UIButton {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
